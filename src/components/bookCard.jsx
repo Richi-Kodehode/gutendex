fdc615f63dac90f13f3b05530f507fd8a883b2c2
Richi-Kodehode <richard25sfj@gmail.com>
2025-03-03 14:47:02 +0100

    small reverts

--- src/components/bookCard.jsx
+++ src/components/bookCard.jsx
@@ -1,20 +1,14 @@
 import { FaStar } from "react-icons/fa";
 
-export function BookSection() {
-  return div    
-}
-
 export default function BookCard() {
   return (
     <div className="bg-gray-400 h-96 flex justify-center ">
       <div className="bg-gray-600 flex flex-col border-2 rounded-xl">
-        <h2>{title}</h2>
-        <img src="#" alt="Picture here" />
-        <h3>{Author}</h3>
-        <p>{dowload_count}</p>
-        <p>Category</p>
-        <p>Language</p>
-        <p>Link to book</p>
+        <img src="#" alt="IMAGE" />
+        <h3>title</h3>
+        <p>author</p>
+        <p>summaries</p>
+        <p>download_count</p>
         <button>
           <FaStar
             style={{
