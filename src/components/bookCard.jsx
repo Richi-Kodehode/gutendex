7771bb3ae7504a6dc8ed8b1c7f1e982ac9d4485d
Richi-Kodehode <richard25sfj@gmail.com>
2025-03-03 14:54:08 +0100

    small design changes

--- src/components/bookCard.jsx
+++ src/components/bookCard.jsx
@@ -2,8 +2,8 @@ import { FaStar } from "react-icons/fa";
 
 export default function BookCard() {
   return (
-    <div className="bg-gray-400 h-96 flex justify-center ">
-      <div className="bg-gray-600 flex flex-col border-2 rounded-xl">
+    <div className="bg-gray-400 min-h-screen flex justify-center ">
+      <div className="bg-gray-600 flex flex-col rounded-xl ring-3 h-96 mt-10">
         <img src="#" alt="IMAGE" />
         <h3>title</h3>
         <p>author</p>
